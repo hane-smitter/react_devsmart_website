7838bcee563017ed93b8a5a240209a01f8605ae0
jab <jab@zoom.zach.com>
2021-07-16 13:04:39 +0300

    map location in contact

--- src/components/section-components/contact-info.js
+++ src/components/section-components/contact-info.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import sectiondata from '../../data/sections.json';
 import parse from 'html-react-parser';
 
-
 class Contact_Info extends Component {
     render() {
         return <div>
@@ -11,8 +10,7 @@ class Contact_Info extends Component {
                 <div className="map-area-wrap">
                     <div className="row no-gutters">
                         <div className="col-lg-8">
-                        <iframe title="riyaqas-map" width="1075" height="440" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=London%2C%20United%20Kingdom+(Title)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
-                        
+                            {parse('<div class="mapouter"><div class="gmap_canvas"><iframe width="1075" height="440" id="gmap_canvas" src="https://maps.google.com/maps?q=emmanuel%20House,%20Ongata%20Rongai,%20Kenya&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://putlocker-is.org"></a><br/><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net">google maps on websites</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>')}
                         </div>  
                         <div className="col-lg-4 desktop-center-item">
                             <div>
