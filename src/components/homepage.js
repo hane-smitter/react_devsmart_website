b6c729552743e94ffcd41acf4d305f27293a9712
hane-smitter <hanesmitter3@gmail.com>
2021-07-29 12:32:26 +0300

    text changes

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,8 +4,8 @@ import SliderV3 from './section-components/slider-v3';
 import ServiceV4 from './section-components/services-v4';
 import About from './section-components/about';
 import Helpv2 from './section-components/help-v2';
-import Marketing from './section-components/marketing';
-import MarketingV2 from './section-components/marketing-v2';
+// import Marketing from './section-components/marketing';
+// import MarketingV2 from './section-components/marketing-v2';
 // import Testimonialv4 from './section-components/testimonial-v4';
 // import Product from './section-components/product';
 // import Testimonial from './section-components/testimonial-v2';
@@ -19,8 +19,8 @@ const HomePage = () => {
         <ServiceV4 />
         <About />
         <Helpv2 />
-        <Marketing customclass="pd-top-120"/>
-        <MarketingV2 />
+        {/* <Marketing customclass="pd-top-120"/>
+        <MarketingV2 /> */}
         {/* <Product />
         <Testimonial />
         <Subscribe /> */}
